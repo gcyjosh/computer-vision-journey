60b4bdda686595f78ef0ced501553d246b95d43d
gcyjosh <gcy.josh@gmail.com>
2026-05-19 21:51:28 +0800

    Update videocapture (180526) .py

--- videocapture (180526) .py
+++ videocapture (180526) .py
@@ -10,13 +10,16 @@ video = cv2.VideoCapture(video_path)
 #visualise video
 
 ret = True 
-while ret: 
+while ret:  
     ret, frame = video.read() #True if frame is read
     
     if ret:
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', frame) #Opens a window titled 'frame'. Shows the current video frame.
         cv2.waitKey(40) #ask cv2 to wait 40ms while it recognises every frame in a 25fps video
 
 # Clean up
-video.release()
-cv2.destroyAllWindows()
+video.release() #Closes the video file. Frees memory and resources
+cv2.destroyAllWindows() #Closes the video display window.
+
+if cv2.waitKey(40) & 0xFF == ord('q'): #This lets you stop the video early by pressing q.
+    break
